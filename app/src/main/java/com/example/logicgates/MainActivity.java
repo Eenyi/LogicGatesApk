62aea53027cd5e9874f63c8741bf1ceb2af726d8
Syed Inshal <bitf19a004@pucit.edu.pk>
2022-11-13 10:25:19 +0500

    C-6-MCQs Now Implimented

--- app/src/main/java/com/example/logicgates/MainActivity.java
+++ app/src/main/java/com/example/logicgates/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.logicgates;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,13 +18,16 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     /*Declaring Varialbes*/
     Button generate, input1, input2, output;
-    ImageView gateImage;
+    ImageView gateImage, resultAvatar;
+    TextView name, success, failure;
+    Button[] optionButtons = new Button[6];
     int [] arrDrawable = {R.drawable.and, R.drawable.or, R.drawable.nand, R.drawable.nor, R.drawable.not, R.drawable.xor},
     options = {R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6},
     colors = {R.drawable.color_red, R.drawable.color_blue, R.drawable.color_green, R.drawable.color_yellow, R.drawable.color_purple, R.drawable.color_magenta, R.drawable.color_brown, R.drawable.color_gold, R.drawable.color_orange};
     String [] optionText = {"AND", "OR", "NAND", "NOR", "NOT", "XOR"};
+    String currentGateImage = optionText[0];
     Random rand = new Random();
-    int selectedGateIndex, optionRandomIndex;
+    int selectedGateIndex, optionRandomIndex, successScore = 0, failureScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*get by id*/
         generate = findViewById(R.id.generate);
+        resultAvatar = findViewById(R.id.resultAvatar);
         gateImage = findViewById(R.id.gateImage);
+        name = findViewById(R.id.name);
+        success = findViewById(R.id.success);
+        failure = findViewById(R.id.failure);
         input1 = findViewById(R.id.input1);
         input2 = findViewById(R.id.input2);
         /*button listener*/
@@ -45,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 generate.setBackgroundResource(colors[rand.nextInt(9)]);
+                resultAvatar.setImageResource(R.drawable.guess);
+                name.setText(" ");
                 /*Code to Set Options on Buttons*/
                 List<String> randOption = new ArrayList<String>();
                 while(randOption.size() != 6) {
@@ -62,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 /*Generate Button Functionality to change Gates Images*/
                 selectedGateIndex = rand.nextInt(6);
                 gateImage.setImageResource(arrDrawable[selectedGateIndex]);
+                currentGateImage = optionText[selectedGateIndex];
                 /*Special NOT Case handle*/
                 if (selectedGateIndex == 4){
                     input1.setVisibility(View.INVISIBLE);
@@ -72,6 +80,33 @@ public class MainActivity extends AppCompatActivity {
                     input1.setVisibility(View.VISIBLE);
                     lp.bottomMargin=notBottomMargin-100;
                     input2.setLayoutParams(lp);
+                }
+            }
+        });
+
+        /*MCQ Options Handle*/
+        optionSelection(0);
+        optionSelection(1);
+        optionSelection(2);
+        optionSelection(3);
+        optionSelection(4);
+        optionSelection(5);
+    }
+    public void optionSelection(int index) {
+        optionButtons[index] = findViewById(options[index]);
+        optionButtons[index].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tempText = (String) optionButtons[index].getText();
+                if (tempText.equals(currentGateImage)) {
+                    resultAvatar.setImageResource(R.drawable.r);
+                    name.setText("Yes, I'm "+ currentGateImage +" Gate");
+                    success.setText("Success: "+ ++successScore);
+                }
+                else {
+                    resultAvatar.setImageResource(R.drawable.w);
+                    name.setText("No, I'm "+ currentGateImage + " Gate");
+                    failure.setText("Failure: "+ ++failureScore);
                 }
             }
         });
