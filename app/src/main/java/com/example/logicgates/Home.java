b3fa2a0a06fd97abe9b2f539143fd4d9bdb0588a
Syed Inshal <bitf19a004@pucit.edu.pk>
2023-01-14 01:20:10 +0500

    Git hub repo

--- app/src/main/java/com/example/logicgates/Home.java
+++ app/src/main/java/com/example/logicgates/Home.java
@@ -2,6 +2,8 @@ package com.example.logicgates;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,14 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-
+        Intent intent;
+        Uri uri;
+        switch (v.getId()) {
+            case R.id.lggithub:
+                uri = Uri.parse("https://github.com/Eenyi/LogicGatesApk.git");
+                intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                break;
+        }
     }
 }
