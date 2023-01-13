e15fcd5893637e2f158ac941f178796effd6697e
Syed Inshal <bitf19a004@pucit.edu.pk>
2023-01-14 00:44:16 +0500

    Home Page done

--- app/src/main/java/com/example/logicgates/Home.java
+++ app/src/main/java/com/example/logicgates/Home.java
@@ -3,12 +3,21 @@ package com.example.logicgates;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
-
+public class Home extends AppCompatActivity implements View.OnClickListener {
+    Button github;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        github = findViewById(R.id.lggithub);
+        github.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
